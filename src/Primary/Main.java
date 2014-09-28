6a4e993ad757a117c9dd9549877ba9af7ba0a07a
Vitor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2014-09-28 20:16:44 -0300

    Re-enable frame limiter

--- src/Primary/Main.java
+++ src/Primary/Main.java
@@ -12,9 +12,9 @@ public class Main {
 
     public static void main(String[] args) {
 		app = new T4Base();
-       // AppSettings newSetting = new AppSettings(true);
-       // newSetting.setFrameRate(120);
-       // app.setSettings(newSetting);
+        AppSettings newSetting = new AppSettings(true);
+        newSetting.setFrameRate(120);
+        app.setSettings(newSetting);
 		app.start();
 	}
 }
