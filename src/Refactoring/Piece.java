0efce095f04d516f7085a035540c40a795741771
Vítor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2014-09-29 16:50:17 -0300

    Start Refactoring

---
+++ src/Refactoring/Piece.java
@@ -0,0 +1,12 @@
+package Refactoring;
+
+import com.jme3.scene.Node;
+
+/**
+ * T4
+ * Created by BlackPearl & HeavenVolkoff & yKane
+ */
+
+public abstract class Piece extends Node implements Cloneable {
+
+}
